f4ca9c4e7e2d4e70f511ee9d27c3a602a307cbe0
nshakib <nazmus.shakib.cse@gmail.com>
2022-08-28 01:09:18 +0600

    React todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
+import TodoList from './components/TodosList';
+
 import './App.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    const [input, setInput] = useState("");// for using string value -> 
+    const [todos, setTodos] = useState([]);//for using array
+    const [editTodo, setEditTodo] = useState(null);
+  return ( 
+    <div className='container'>
+        <div className='app-wrapper'>
+            <div>
+                <Header />
+            </div>
+            <div>
+                <Form
+                    input = {input} //props
+                    setInput = {setInput}
+                    todos = {todos}
+                    setTodos = {setTodos}
+                    editTodo = {editTodo}
+                    setEditTodo = {setEditTodo}
+                />
+            </div>
+            <div>
+                <TodoList 
+                    todos={todos}
+                    setTodos={setTodos}
+                    setEditTodo={setEditTodo}
+                    />
+            </div>
+        </div>
     </div>
-  );
+   );
 }
-
+ 
 export default App;
+
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Header = () => {
+    return ( 
+        <div className="header">
+            <h1>Todos-List</h1>
+        </div>
+     );
+}
+ 
+export default Header;
